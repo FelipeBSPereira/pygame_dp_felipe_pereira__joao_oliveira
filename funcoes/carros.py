dc877516fd0c52d3cc182854ceb4633e51050650
oli-ver-28 <joaoaso@al.insper.edu.br>
2024-11-11 10:07:36 -0300

    movimento1

--- funcoes/carros.py
+++ funcoes/carros.py
@@ -14,3 +14,22 @@ class carros(py.sprite.Sprite):
         self.image = self.carro1
         self.rect = self.image.get_rect()
         self.mask= py.mask.from_surface(self.image)
+
+    def update (self):
+        self.movement()
+        self.correction()
+        self.Checaimpacto()
+        self.rect.center = (self.x,self.y)
+    
+    def movement (self):
+        keys = py.key.get_pressed()
+        if keys [py.K_LEFT]:
+            self.x -= self.vel
+            self.image = self.capy2
+        elif keys [py.K_RIGHT]:
+            self.x += self.vel
+            self.image = self.capy1
+        if keys [py.K_UP]:
+            self.y -= self.vel
+        elif keys [py.K_DOWN]:
+            self.y += self.vel
