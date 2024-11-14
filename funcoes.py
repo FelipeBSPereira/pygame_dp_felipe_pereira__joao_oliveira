7a020bbdc16d8bc36bcb50b4963a2ccebe8ac58f
FelipeBSPereira <felipebsp@al.insper.edu.br>
2024-11-14 08:42:52 -0300

    imagem fundo teste

--- funcoes.py
+++ funcoes.py
@@ -129,8 +129,8 @@ class Torta(py.sprite.Sprite):
 class Tela(py.sprite.Sprite):
     def _init_(self):
         super()._init_()
-        self.t_jogo = py.image.load ('tela_jogo.png')
-        self.t_final = py.image.load ('tela_final.png')
+        self.t_jogo = py.image.load ('assets/fundo_teste.png')
+        #self.t_final = py.image.load ('tela_final.png')
 
         self.t_jogo = py.transform.scale (self.t_jogo,(WIDTH,HEIGHT))
         self.t_final = py.transform.scale (self.t_final,(WIDTH,HEIGHT))
