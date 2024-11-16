6b69516773116c2c26701a466420600742dd6a30
FelipeBSPereira <felipebsp@al.insper.edu.br>
2024-11-16 17:49:14 -0300

    Correção parametros

--- parametros2.py
+++ parametros2.py
@@ -1,63 +1,33 @@
 import pygame as py
-import time
 
 WIDTH = 1400
-HEIGHT = 800
+HEIGHT = 700
 
 py.init()
 
-window= py.display.set_mode((WIDTH,HEIGHT))
-py.display.set_caption ('Crossy Pica-pau')
-clock = py.time.Clock ()
+
+window = py.display.set_mode((WIDTH, HEIGHT))
+py.display.set_caption('Crossy Pica-pau')
+clock = py.time.Clock()
+
+
+pontos_font = py.font.SysFont('comicsans', 44, True)
+final_font = py.font.SysFont('comicsans', 64, True)
 
 pontos = 0
-pontos_font = py.font.SysFont ('comicsans', 44 , True)
+nivel = 1
+recorde = 0
+jogo = True
 
-fundo = Tela ()
+
+#py.mixer.init()
+#som_jogo = py.mixer.Sound('musica_fundo.mp3')
+#som_torta = py.mixer.Sound('som_torta.mp3')
+#som_perda = py.mixer.Sound('som_perda.mp3')
+
+#py.mixer.Sound.play(som_jogo, loops=-1)
+
 screen_group = py.sprite.Group()
-screen_group.add(fundo)
-
-picapau =picapau()
 grupo_picapau = py.sprite.Group()
-grupo_picapau.add(picapau)
-
-zeca_urubu = viloes(1)
-leoncio = viloes(2)
-grupo_picapau = py.sprite.Group()
-grupo_viloes.add(carro_1,carro_2)
-
-torta1= Torta(1)
-torta2= torta(2)
-grupo_torta=py.sprite.Group()
-grupo_torta.add(torta1,torta2)
-tortas= [torta1,torta2]
-
-somJG= py.mixer.Sound("música_jogo")
-somD= py.mixer.Sound("perdeu")
-somR= py.mixer.Sound("risada")
-
-
-captura= captura()
-jogo=True
-game = True
-
-while game :
-    
-    clock.tick(60)
-    for event in py.event.get():
-        if event.type == py.QUIT:
-            game = False
-
-    screen_group.draw(window)
-
-    grupo_viloes.draw(window)
-    grupo_picapau.draw(window)
-    grupo_torta.draw(window)
-    grupo_viloes.update()
-    grupo_picapau.update()
-    grupo_torta.update()
-    screen_group.update()
-    
-
-    py.display.update()
-py.quit()
+grupo_viloes = py.sprite.Group()
+grupo_torta = py.sprite.Group()
