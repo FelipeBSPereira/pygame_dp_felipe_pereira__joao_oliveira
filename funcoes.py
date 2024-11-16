a85c43e12eb6e3f8a431214ded21b80ef94d041a
FelipeBSPereira <felipebsp@al.insper.edu.br>
2024-11-16 18:59:23 -0300

    Telas inicial e principal

--- funcoes.py
+++ funcoes.py
@@ -139,148 +139,29 @@ class Torta(py.sprite.Sprite):
             self.lado = 1
             self.x = 130
 
-class Tela(py.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.t_jogo = py.image.load ('assets/fundo_teste.png')
-        self.t_final = py.image.load ('assets/fundo_teste.png')
-
-        self.t_jogo = py.transform.scale (self.t_jogo,(WIDTH,HEIGHT))
-        self.t_final = py.transform.scale (self.t_final,(WIDTH,HEIGHT))
-
-        self.image = self.t_jogo
-        print(self.image)
-        self.x = 0
-        self.y = 0
-        self.rect = self.image.get_rect()
-
-        def arruma (self):
-            self.rect.topleft = (self.x,self.y)
-
-def sobenivel():
-    global nivel, pontos
-    nivel += 1
-    for viloes in  grupo_viloes:
-        if viloes.vel < 0:
-            viloes.vel -= 1
-        else:
-            viloes.vel += 1
-def tela_fim():
-    global pontos, recorde, jogo
-    texto_final= pontos_font.render("Pontuação: " + str(pontos), True, (255, 0, 0))
-    texto_recorde = pontos_font.render("Recorde: " + str(recorde), True, (0, 255, 0))
-    texto_reiniciar = pontos_font.render("Pressione R para reiniciar", True, (255, 255, 255))
+def tela_inicial():
     window.fill((0, 0, 0))
-    window.blit(texto_final, (WIDTH / 2 - texto_final.get_width() / 2, HEIGHT / 2 - 50))
-    window.blit(texto_recorde, (WIDTH / 2 - texto_recorde.get_width() / 2, HEIGHT / 2))
-    window.blit(texto_reiniciar, (WIDTH / 2 - texto_reiniciar.get_width() / 2, HEIGHT / 2 + 50))
-    py.display.flip()
-    while not jogo:
+    titulo = final_font.render("Bem-vindo ao Crossy Pica-pau!", True, (255, 255, 0))
+    iniciar_texto = pontos_font.render("Pressione qualquer tecla para começar", True, (255, 255, 255))
+    window.blit(titulo, (WIDTH // 2 - titulo.get_width() // 2, HEIGHT // 3))
+    window.blit(iniciar_texto, (WIDTH // 2 - iniciar_texto.get_width() // 2, HEIGHT // 2))
+    py.display.update()
+
+    esperando = True
+    while esperando:
         for event in py.event.get():
             if event.type == py.QUIT:
                 py.quit()
                 exit()
-            if event.type == py.KEYDOWN and event.key == py.K_r:
-                reinicia_jogo()
-def reinicia_jogo():
-    global pontos, nivel, jogo
-    pontos = 0
-    nivel = 1
-    jogo = True
-    picapau.x = 50
-    picapau.y = HEIGHT / 2
-    for viloes in grupo_viloes:
-        viloes.y = HEIGHT / 2
-        viloes.vel = 4  
+            if event.type == py.KEYDOWN:
+                esperando = False
 
-# def main():
-#     global pontos, jogo
-#     pontos_alvo = 5 
-#     while True:
-#         clock.tick(60)
-#         for event in py.event.get():
-#             if event.type == py.QUIT:
-#                 py.quit()
-#                 exit()
-
-#         if jogo:
-#             window.fill((255, 255, 255)) 
-#             grupo_picapau.update()
-#             grupo_viloes.update()
-#             grupo_picapau.draw(window)
-#             grupo_viloes.draw(window)
-#             pontos_texto = pontos_font.render("Pontuação: " + str(pontos), True, (0, 0, 0))
-#             nivel_texto = pontos_font.render("Nível: " + str(nivel), True, (0, 0, 0))
-#             window.blit(pontos_texto, (10, 10))
-#             window.blit(nivel_texto, (10, 60))
-#             py.display.update()
-#             pontos += 1
-#             if pontos >= pontos_alvo * nivel:
-#                 sobenivel()
-#         else:
-#             tela_fim()
-
-picapau1 = picapau()
-grupo_picapau = py.sprite.Group(picapau1)
-zeca_urubu = viloes(1)
-leoncio = viloes(2)
-grupo_viloes = py.sprite.Group(zeca_urubu, leoncio)
+class Tela(py.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.t_jogo = py.image.load('mapa1.png').convert_alpha()
+        self.t_jogo = py.transform.scale(self.t_jogo, (WIDTH, HEIGHT))
+        self.image = self.t_jogo
+        self.rect = self.image.get_rect()
 
 
-# py.init()
-
-# window= py.display.set_mode((WIDTH,HEIGHT))
-# py.display.set_caption ('Crossy Pica-pau')
-# clock = py.time.Clock ()
-
-# pontos = 0
-# pontos_font = py.font.SysFont ('comicsans', 44 , True)
-
-# fundo = Tela ()
-# screen_group = py.sprite.Group()
-# screen_group.add(fundo)
-
-# picapau =picapau()
-# grupo_picapau = py.sprite.Group()
-# grupo_picapau.add(picapau)
-
-# zeca_urubu = viloes(1)
-# leoncio = viloes(2)
-# grupo_picapau = py.sprite.Group()
-# grupo_viloes.add(leoncio,zeca_urubu)
-
-# torta1= Torta(1)
-# torta2= Torta(2)
-# grupo_torta=py.sprite.Group()
-# grupo_torta.add(torta1,torta2)
-# tortas= [torta1,torta2]
-
-# #somJG= py.mixer.Sound("música_jogo")
-# #somD= py.mixer.Sound("perdeu")
-# #somR= py.mixer.Sound("risada")
-
-
-# #captura= capture() 
-# jogo=True
-# game = True
-
-# while game :
-    
-#     clock.tick(60)
-#     for event in py.event.get():
-#         if event.type == py.QUIT:
-#             game = False
-
-#     screen_group.draw(window)
-
-#     grupo_viloes.draw(window)
-#     grupo_picapau.draw(window)
-#     grupo_torta.draw(window)
-#     grupo_viloes.update()
-#     grupo_picapau.update()
-#     grupo_torta.update()
-#     screen_group.update()
-    
-
-#     py.display.update()
-# py.quit()
